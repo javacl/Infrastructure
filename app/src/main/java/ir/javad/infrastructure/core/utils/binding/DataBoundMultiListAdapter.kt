ee0cde456690965c0e84f55b67a05f16d4d02fee
javad <javad.sa@part.ir>
2019-10-08 14:42:38 +0330

    recyclerViews Util created

---
+++ app/src/main/java/ir/javad/infrastructure/core/utils/binding/DataBoundMultiListAdapter.kt
@@ -0,0 +1,28 @@
+package ir.javad.infrastructure.core.utils.binding
+
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+
+abstract class DataBoundMultiListAdapter<T, V : ViewDataBinding>(
+    diffCallback: DiffUtil.ItemCallback<T>
+) : ListAdapter<T, DataBoundViewHolder<V>>(
+    AsyncDifferConfig.Builder<T>(diffCallback)
+        .build()
+) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBoundViewHolder<V> {
+        val binding = createBinding(parent, viewType)
+        return DataBoundViewHolder(binding)
+    }
+
+    protected abstract fun createBinding(parent: ViewGroup, viewType: Int): V
+
+    override fun onBindViewHolder(holder: DataBoundViewHolder<V>, position: Int) {
+        bind(holder.binding, position)
+        holder.binding.executePendingBindings()
+    }
+
+    protected abstract fun bind(binding: V, position: Int)
+}
---
+++ app/src/main/java/ir/javad/infrastructure/core/utils/binding/DataBoundViewHolder.kt
@@ -0,0 +1,7 @@
+package ir.javad.infrastructure.core.utils.binding
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class DataBoundViewHolder<out T : ViewDataBinding> constructor(val binding: T) :
+    RecyclerView.ViewHolder(binding.root)
