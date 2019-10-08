9929b4b79f0af2743e0ed1e233fbb2b2c046caeb
javad <javad.sa@part.ir>
2019-10-08 15:22:14 +0330

    BaseDialog created

---
+++ app/src/main/java/ir/javad/infrastructure/core/base/BaseDialog.kt
@@ -0,0 +1,88 @@
+package ir.javad.infrastructure.core.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.core.view.ViewCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ir.javad.infrastructure.R
+import ir.javad.infrastructure.R.style
+import javax.inject.Inject
+
+abstract class BaseDialog<T : ViewDataBinding, V : ViewModel> :
+    DialogFragment() {
+
+    @Inject
+    lateinit var mViewModelFactory: ViewModelProvider.Factory
+
+    lateinit var mViewDataBinding: T
+    lateinit var mViewModel: V
+
+    abstract val bindingVariable: Int
+    abstract val viewModel: V
+    @get:LayoutRes
+    abstract val layoutId: Int
+
+    abstract fun beforeView()
+    abstract fun afterView(savedInstanceState: Bundle?)
+    abstract fun destroyView()
+    abstract fun resumeView()
+    abstract fun pauseView()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(
+            STYLE_NO_FRAME,
+            style.DialogTheme
+        )
+        mViewModel = viewModel
+        beforeView()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mViewDataBinding =
+            DataBindingUtil.inflate(inflater, layoutId, container, false)
+        val view = mViewDataBinding.root
+        ViewCompat.setLayoutDirection(
+            view,
+            ViewCompat.LAYOUT_DIRECTION_LTR
+        )
+        return view
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        mViewDataBinding.setVariable(bindingVariable, mViewModel)
+        mViewDataBinding.lifecycleOwner = this
+        mViewDataBinding.executePendingBindings()
+        afterView(savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        destroyView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        resumeView()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pauseView()
+    }
+}
